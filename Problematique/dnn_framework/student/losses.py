f24c195cc7dac0bcd756c174b7e363f5510f3e78
Edouard5618 <edouard5618@gmail.com>
2025-09-03 19:01:32 -0400

    Implement loss functions

--- Problematique/dnn_framework/student/losses.py
+++ Problematique/dnn_framework/student/losses.py
@@ -14,7 +14,18 @@ class CrossEntropyLoss(Loss):
         :param target: The target classes (shape: (N,))
         :return A tuple containing the loss and the gradient with respect to the input (loss, input_grad)
         """
-        raise NotImplementedError()
+        N = target.shape[0]
+        softmaxed_input = softmax(x)
+        target_onehot = np.eye(x.shape[1])[target]
+
+        # Compute cross-entropy loss
+        L = -np.sum(target_onehot * np.log(softmaxed_input), axis=1)  # sum over classes
+        loss = np.mean(L)  # average over batch
+
+        # Gradient
+        input_grad = (softmaxed_input - target_onehot) / N
+
+        return loss, input_grad
 
 
 def softmax(x):
@@ -22,7 +33,10 @@ def softmax(x):
     :param x: The input tensor (shape: (N, C))
     :return The softmax of x
     """
-    raise NotImplementedError()
+    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    y = exp_x / np.sum(exp_x, axis=1, keepdims=True)
+    return y
+
 
 
 class MeanSquaredErrorLoss(Loss):
@@ -36,4 +50,7 @@ class MeanSquaredErrorLoss(Loss):
         :param target: The target tensor (shape: same as x)
         :return A tuple containing the loss and the gradient with respect to the input (loss, input_grad)
         """
-        raise NotImplementedError()
+        N = x.size
+        L = np.sum((x-target)**2)/N
+        dL_dx = 2/N*(x-target)
+        return L, dL_dx
